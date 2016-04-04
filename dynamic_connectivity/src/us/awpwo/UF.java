90d4f4f10f3ec71160f458b344c419b9a8ff6542
student <student@10.2.3.9>
2016-04-04 15:07:15 +0200

    kolejna zmiana w UF

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -11,11 +11,25 @@ public class UF {
 		}
 	}
 	
-	public void union(int p, int q) {
+	private int root(int i)
+	{
+		while (i != id[i])
+		{
+			i = id[i];
+		}
+		return i;
 	}
 	
-	public boolean connected(int p, int q) {
-		return false;
+	public boolean connected(int p, int q)
+	{
+		return root(p) == root(q);
+	}
+	
+	public void union(int p, int q)
+	{
+		int i = root(p);
+		int j = root(q);
+		id[i] = j;
 	}
 	
 }
