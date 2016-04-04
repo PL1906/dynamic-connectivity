1675d7bc2daecfce5fcd91e3952869c7ae68892b
student <student@10.2.3.9>
2016-04-04 15:03:53 +0200

    zmiana pliku UF

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -11,11 +11,22 @@ public class UF {
 		}
 	}
 	
-	public void union(int p, int q) {
+	public boolean connected(int p, int q)
+	{
+		 return id[p] == id[q];
 	}
 	
-	public boolean connected(int p, int q) {
-		return false;
+	public void union(int p, int q)
+	{
+		int pid = id[p];
+		int qid = id[q];
+		for (int i = 0; i < id.length; ++i)
+		{
+			 if (id[i] == pid)
+			 {
+				 id[i] = qid;
+			 }
+		}
 	}
 	
 }
